200187df3818ecb178db9d5b2c74b5838e733dd9
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-02-25 01:15:20 +0545

    feat(backend): add Plan model schema for database

---
+++ alaya-backend/models/Plan.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../database/db");
+
+const Plan = sequelize.define("Plan", {
+  id: { 
+    type: DataTypes.INTEGER, 
+    autoIncrement: true, 
+    primaryKey: true 
+  },
+  userId: { 
+    type: DataTypes.INTEGER, 
+    allowNull: false 
+  },
+  date: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
+  yogaType: { 
+    type: DataTypes.STRING, 
+    allowNull: true 
+  }, 
+  duration: { 
+    type: DataTypes.INTEGER, 
+    defaultValue: 30 
+  },
+  notes: { 
+    type: DataTypes.TEXT, 
+    allowNull: true 
+  },
+}, { 
+  tableName: "plans", 
+  timestamps: true 
+});
+
+module.exports = Plan;
