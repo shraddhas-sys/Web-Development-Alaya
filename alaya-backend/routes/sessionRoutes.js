d06732fcbe669ae188aec2371417ea536339343a
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-03-04 20:58:19 +0545

    feat: add session routes

--- alaya-backend/routes/sessionRoutes.js
+++ alaya-backend/routes/sessionRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { addSession, getSessions } = require("../controllers/sessionController");
+const auth = require("../middleware/auth");
 
-router.post("/add", addSession);
-router.get("/all/:userId", getSessions);
+router.post("/add", auth, addSession);
+router.get("/all/:userId", auth, getSessions);
+router.patch("/:id/complete", auth, require("../controllers/sessionController").completeSession);
+router.delete("/:id", auth, require("../controllers/sessionController").deleteSession);
 
 module.exports = router;
