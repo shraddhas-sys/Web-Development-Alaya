77010af6131ece2e725ae3a6b4b1c2f9ecab7ffd
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-03-04 20:55:57 +0545

    feat: update recipe model schema

--- alaya-backend/models/Recipe.js
+++ alaya-backend/models/Recipe.js
@@ -3,23 +3,50 @@ const { sequelize } = require("../database/db");
 
 const Recipe = sequelize.define("Recipe", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  
   userId: { 
     type: DataTypes.INTEGER, 
-    allowNull: false,
+    allowNull: true, 
     field: 'userid' 
   },
+  
   name: { type: DataTypes.STRING, allowNull: false },
+  
+  ingredients: { 
+    type: DataTypes.JSON, 
+    allowNull: true 
+  },
+  
+  calories: { 
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+
+  instructions: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+
   isFavorite: { 
     type: DataTypes.BOOLEAN, 
     defaultValue: false,
     field: 'isfavorite' 
+  },
+
+ // to check admin made or user
+  isAdminRecipe: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
   }
 }, { 
   tableName: "recipes", 
   timestamps: true, 
-  underscored: true,
-  createdAt: 'createdAt',
-  updatedAt: 'updatedAt'
+  underscored: true
 });
 
 module.exports = Recipe;
