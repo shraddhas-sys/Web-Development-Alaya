7a6ef0498507432f98d874d361c50c7a5452f98b
shraddhas-sys <shraddhabasnet28@gmail.com>
2026-03-04 21:02:52 +0545

    feat: update MealPlan model schema

--- alaya-backend/models/MealPlan.js
+++ alaya-backend/models/MealPlan.js
@@ -3,12 +3,28 @@ const { sequelize } = require("../database/db");
 
 const MealPlan = sequelize.define("MealPlan", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  userId: { type: DataTypes.INTEGER, allowNull: false },
+  userId: { 
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    references: {
+      model: 'users', 
+      key: 'id'
+    }
+  },
   date: { type: DataTypes.DATEONLY, allowNull: false },
+  // meal id list
+  morningMealId: { type: DataTypes.INTEGER, allowNull: true },
+  afternoonMealId: { type: DataTypes.INTEGER, allowNull: true },
+  eveningMealId: { type: DataTypes.INTEGER, allowNull: true },
+
   breakfast: { type: DataTypes.STRING },
   lunch: { type: DataTypes.STRING },
   dinner: { type: DataTypes.STRING },
+  
   notes: { type: DataTypes.TEXT }
-}, { tableName: "meal_plans" });
+}, { 
+  tableName: "meal_plans",
+  timestamps: true 
+});
 
 module.exports = MealPlan;
